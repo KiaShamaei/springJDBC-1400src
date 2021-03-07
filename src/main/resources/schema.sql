62c2a6cb1e3dd27bc8c8e9ce1a9bee00df857001
Shamaei <Shamaei.Kiarash@TaraTechGroup.com>
2021-03-07 20:03:56 +0330

    springbootH2

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee (
+id int not null primary key ,
+first_name varchar(255) not null,
+last_name varchar(255) not null,
+address varchar(255) not null
+
+)
+
